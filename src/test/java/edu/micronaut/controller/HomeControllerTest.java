6809f0ec019037f54ad78eab8192d5a1b996b6c6
Maryana Kravets <marjana_s@ukr.net>
2019-09-11 15:27:39 +0300

    reworked the tests

--- src/test/java/edu/micronaut/controller/HomeControllerTest.java
+++ src/test/java/edu/micronaut/controller/HomeControllerTest.java
@@ -7,11 +7,12 @@ import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.RxHttpClient;
 import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.runtime.server.EmbeddedServer;;
+import io.micronaut.security.authentication.UsernamePasswordCredentials;
+import io.micronaut.security.token.jwt.render.BearerAccessRefreshToken;
 import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-
-import javax.inject.Inject;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -19,58 +20,45 @@ import static org.junit.jupiter.api.Assertions.fail;
 @MicronautTest
 public class HomeControllerTest {
 
-    @Inject
-    EmbeddedServer embeddedServer;
+    private  static EmbeddedServer embeddedServer;
+
+    private static RxHttpClient rxHttpClient;
 
 
+    @BeforeEach
+    public  void  setup(){
+        embeddedServer= ApplicationContext.build().run(EmbeddedServer.class);
+        rxHttpClient=embeddedServer.getApplicationContext().createBean(RxHttpClient.class,embeddedServer.getURL());
+    }
 
-   // private  static EmbeddedServer embeddedServer;
-
-    //private static RxHttpClient rxHttpClient;
-
-////
-//    @BeforeClass
-//    public static  void  setup(){
-//        embeddedServer= ApplicationContext.build().run(EmbeddedServer.class);
-//        rxHttpClient=embeddedServer.getApplicationContext().createBean(RxHttpClient.class,embeddedServer.getURL());
-//    }
-////
-//    @AfterClass
-//    public  static  void cleanup(){
-//        if(embeddedServer!=null) {
-//            embeddedServer.stop();
-//        }
-//        if (rxHttpClient!=null){
-//            rxHttpClient.stop();
-//        }
-//    }
-//
-//    @Test
-//    public void canEmailWith() throws Exception{
-//        UsernamePasswordCredentials upc=new UsernamePasswordCredentials("user","password");
-//        HttpRequest<Object> loginRequest=HttpRequest.POST("/login", upc);
-//        HttpResponse<BearerAccessRefreshToken> rsp=rxHttpClient.toBlocking().exchange(loginRequest,BearerAccessRefreshToken.class);
-//        assertEquals(rsp.getStatus(),HttpStatus.OK);
-//        assertEquals("user",rsp.getBody().get().getUsername());
-//    }
-//
-////    @Test
-////    public  void cantLogin(){
-////        try{
-////            UsernamePasswordCredentials upc=new UsernamePasswordCredentials("user","pass");
-////            HttpRequest loginRequest=HttpRequest.POST("/login",upc);
-////            HttpResponse<BearerAccessRefreshToken>rsp=rxHttpClient.toBlocking().exchange(loginRequest,BearerAccessRefreshToken.class);
-////            fail();
-////        }catch (HttpClientResponseException ex){
-////            assertEquals(ex.getStatus(),HttpStatus.UNAUTHORIZED);
-////        }
-////    }
-
-
-    @Test
-    public void testIndex() throws Exception {
-        try(RxHttpClient client = embeddedServer.getApplicationContext().createBean(RxHttpClient.class, embeddedServer.getURL())) {
-            assertEquals(HttpStatus.OK, client.toBlocking().exchange("/greet/").status());
+    @AfterAll
+    public static   void cleanup(){
+        if(embeddedServer!=null) {
+            embeddedServer.stop();
+        }
+        if (rxHttpClient!=null){
+            rxHttpClient.stop();
         }
     }
+
+    @Test
+    public void canEmailWith() {
+        UsernamePasswordCredentials upc = new UsernamePasswordCredentials("user", "password");
+        HttpRequest<Object> loginRequest = HttpRequest.POST("/login", upc);
+        HttpResponse<BearerAccessRefreshToken> rsp = rxHttpClient.toBlocking().exchange(loginRequest, BearerAccessRefreshToken.class);
+        assertEquals(rsp.getStatus(),HttpStatus.OK);
+        assertEquals("user",rsp.getBody().get().getUsername());
+    }
+
+    @Test
+    public  void cantLogin(){
+        try{
+            UsernamePasswordCredentials upc=new UsernamePasswordCredentials("user","pass");
+            HttpRequest<Object> loginRequest=HttpRequest.POST("/login",upc);
+            HttpResponse<BearerAccessRefreshToken>rsp=rxHttpClient.toBlocking().exchange(loginRequest,BearerAccessRefreshToken.class);
+            fail();
+        }catch (HttpClientResponseException ex){
+            assertEquals(ex.getStatus(),HttpStatus.UNAUTHORIZED);
+        }
+   }
 }
